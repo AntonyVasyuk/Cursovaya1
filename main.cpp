b72761bee847e50a3324cf68e5c7cd4b570af24b
TAnon <vasyuk.a.a@yandex.ru>
2026-05-24 19:20:59 +0300

    Added displaying coordinates, calculating different integreals, and Function definition (next changong Function to include its function as field, on the way)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <functional>
 
 using std::ifstream;
 using std::ofstream;
@@ -17,13 +18,13 @@ enum class FunctionType
     Quadric,
     Qubic,
     Log,
-    E_x,
+    A_x,
     Sin,
     Cos
 };
 
 
-float calculate_integral_left_rectangle(float (*f)(float), float a, float b, int n)
+float calculate_integral_left_rectangle(std::function<float(float)> f, float a, float b, int n)
 {
     float dx = (b - a) / n;
     //std::cout << dx << std::endl;
@@ -95,43 +96,119 @@ class Function
 {
 private:
     FunctionType type;
+    std::function<float(float)> f;
     float* parameters;
 
 public:
-    Function(FunctionType t, int size, float* params);
-    float calcualate_ranged_integral(float a, float b);
+    Function(FunctionType t, float* params)
+    {
+        this->type = t;
+        int size;
+        switch (t)
+        {
+        case FunctionType::Linear:
+            size = 2;
+
+            break;
+        case FunctionType::Quadric:
+            size = 3;
+            break;
+        case FunctionType::Qubic:
+            size = 4;
+            break;
+        case FunctionType::Sin:
+            size = 4;
+            break;
+        case FunctionType::Cos:
+            size = 4;
+            break;
+        case FunctionType::Log:
+            size = 5;
+            break;
+        case FunctionType::A_x:
+            size = 5;
+            break;
+        }
+
+        this->parameters = new float[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            this->parameters[i] = params[i];
+        }
+    }
+
+    //5648678777777777777777777777777777777777777777777777777777777777777
+    float calcualate_ranged_integral(float x1, float x2, int n)
+    {
+        switch (this->get_type())
+        {
+        case FunctionType::Linear:
+            float k = this->parameters[0], b = this->parameters[1];
+            return calculate_integral_left_rectangle([k, b](float x) {return k * x + b;}, x1, x2, n);
+
+        case FunctionType::Quadric:
+            float a = this->parameters[0], b = this->parameters[1], c = this->parameters[2];
+            return calculate_integral_left_rectangle([a, b, c](float x) {return a * x * x + b * x + c; }, x1, x2, n);
+
+        case FunctionType::Qubic:
+            float a = this->parameters[0], b = this->parameters[1], c = this->parameters[2], d = this->parameters[3];
+            return calculate_integral_left_rectangle([a, b, c, d](float x) {return a * x * x * x + b * x * x + c * x + d; }, x1, x2, n);
+        };
+    }
     FunctionType get_type() { return this->type; }
+
+    void add_graphic_to_vector(std::vector <sf::VertexArray>& v, float x1, float x2, int n)
+    {
+        float dx = (x2 - x1) / n;
+        //std::cout << dx << std::endl;
+        float x_i = x1;
+        float ans = 0;
+        while (x_i < x2) {
+            float x_i1 = x_i + dx;
+            //float dy = f(a, b, c, x_i1) - f(a, b, c, x_i);
+            ans += f(x_i1) * dx;
+            x_i += dx;
+        }
+    }
 };
 
-Function::Function(FunctionType t, int size, float* params)
-{
-    type = t;
-    parameters = new float[size];
 
-    for (int i = 0; i < size; i++)
+class Camera
+{
+private:
+    float x;
+    float y;
+
+public:
+    Camera(float x, float y)
     {
-        parameters[i] = params[i];
+        this->x = x;
+        this->y = y;
     }
-}
 
-float Function::calcualate_ranged_integral(float a, float b)
-{
-    switch (this->get_type())
+    void move(int dx, int dy)
     {
-    case FunctionType::Linear:
-        
-        break;
+        this->x += dx;
+        this->y += dy;
+    }
 
-    case FunctionType::Quadric:
-
-        break;
-    };
-}
+    std::pair<int, int> calculate_position(int x, int y)
+    {
+        return std::make_pair(x - this->x, y - this->y);
+    }
+};
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Graphics");
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Graphics");
+
+    Camera camera(-20, -580);
+    bool moving_camera = false;
+    std::pair<int, int> previous_position;
+
+    std::vector <sf::VertexArray> lines;
 
     while (window.isOpen())
     {
@@ -140,7 +217,55 @@ int main()
         {
             // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
+            {
                 window.close();
+            }
+
+            if (event->is<sf::Event::MouseButtonPressed>())
+            {
+                moving_camera = true;
+                sf::Vector2i v = sf::Mouse::getPosition();
+                previous_position = std::make_pair(v.x, v.y);
+            }
+
+            if (event->is<sf::Event::MouseButtonReleased>())
+            {
+                moving_camera = false;
+            }
+
+            if (moving_camera)
+            {
+                sf::Vector2i v = sf::Mouse::getPosition();
+                camera.move(-v.x + previous_position.first, -v.y + previous_position.second);
+
+                previous_position = std::make_pair(v.x, v.y);
+            }
         }
+
+        window.clear(sf::Color({200, 200, 200}));
+
+        std::pair<int, int> c = camera.calculate_position(0, 0);
+        //circle.setPosition({ (float)c.first, (float)c.second });
+
+        sf::VertexArray line1(sf::PrimitiveType::Lines, 2);
+        line1[0].position = sf::Vector2f((float)c.first - 200, (float)c.second);
+        line1[1].position = sf::Vector2f((float)c.first + 760, (float)c.second);
+
+        line1[0].color = sf::Color({ 200, 200, 200 });
+        line1[1].color = sf::Color({ 0, 0, 0 });
+
+        sf::VertexArray line2(sf::PrimitiveType::Lines, 2);
+        line2[0].position = sf::Vector2f((float)c.first, (float)c.second + 200);
+        line2[1].position = sf::Vector2f((float)c.first, (float)c.second - 560);
+
+        line2[0].color = sf::Color({ 200, 200, 200 });
+        line2[1].color = sf::Color({ 0, 0, 0 });
+
+        window.draw(line1);
+        window.draw(line2);
+
+        //window.draw(circle);
+
+        window.display();
     }
 }
